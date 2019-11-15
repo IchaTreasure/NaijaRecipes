ffbba30442a04d34f0e4744a3181c413c567c66d
IchaTreasure <treasureicha99@gmail.com>
2019-11-15 21:46:43 +0000

    took off code for s3 bucket

--- app.py
+++ app.py
@@ -15,7 +15,12 @@ ACCESS_KEY_ID = os.environ.get('ACCESS_KEY_ID')
 ACCESS_SECRET_KEY = os.environ.get('ACCESS_SECRET_KEY')
 BUCKET_NAME = os.environ.get('BUCKET_NAME')
 
-
+s3 = boto3.resource('s3',
+    aws_access_key_id=ACCESS_KEY_ID,
+    aws_secret_access_key=ACCESS_SECRET_KEY,
+    config=Config(signature_version='s3v4')
+    )
+ 
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = os.environ.get('MONGO_DBNAME')
@@ -96,16 +101,9 @@ def insert_recipes():
     f.save(secure_filename(f.filename))
     filename= secure_filename(f.filename)
     
+    with open( filename, 'rb')  as f:
     
-    data = open( filename, 'rb')  
-    
-    s3 = boto3.resource('s3',
-    aws_access_key_id=ACCESS_KEY_ID,
-    aws_secret_access_key=ACCESS_SECRET_KEY,
-    config=Config(signature_version='s3v4')
-    )
- 
-    s3.Bucket(BUCKET_NAME).put_object(Key=filename, Body=data)
+     s3.Bucket(BUCKET_NAME).put_object(Key=filename, Body=f, ACL='public-read')
 
     Recipies = mongo.db.Recipies
     recipe_dict = request.form.to_dict()
