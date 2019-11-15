51f508ef3f4f3304df01a0d062a1e9ffaad9bbae
IchaTreasure <treasureicha99@gmail.com>
2019-11-15 20:31:59 +0000

    calling images into html from s3 bucket

--- app.py
+++ app.py
@@ -15,11 +15,7 @@ ACCESS_KEY_ID = os.environ.get('ACCESS_KEY_ID')
 ACCESS_SECRET_KEY = os.environ.get('ACCESS_SECRET_KEY')
 BUCKET_NAME = os.environ.get('BUCKET_NAME')
 
-s3 = boto3.resource('s3',
-    aws_access_key_id=ACCESS_KEY_ID,
-    aws_secret_access_key=ACCESS_SECRET_KEY,
-    config=Config(signature_version='s3v4')
-    )
+
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = os.environ.get('MONGO_DBNAME')
@@ -100,9 +96,17 @@ def insert_recipes():
     
     f.save(secure_filename(f.filename))
     filename= secure_filename(f.filename)
+    
+    
+    s3 = boto3.resource('s3',
+    aws_access_key_id=ACCESS_KEY_ID,
+    aws_secret_access_key=ACCESS_SECRET_KEY,
+    config=Config(signature_version='s3v4')
+    )
+    
     with open( filename, 'rb') as f: 
  
-     s3.Bucket(BUCKET_NAME).put_object(Key=filename, Body=f)
+        s3.Bucket(BUCKET_NAME).put_object(Key=filename, Body=f, ACL='public-read')
 
     Recipies = mongo.db.Recipies
     recipe_dict = request.form.to_dict()
